0e97537fa6e54ff3f8ed43ee61a5c1115fb5a656
PankajTurkar <43439911+PankajTurkar@users.noreply.github.com>
2025-01-11 07:31:25 +0000

    datatypes commit by pankaj

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,12 @@
+//Datatypes and ECMA standards | chai aur #javascript
+
+let name1 = "pankaj"
+let age = 20
+let isLogginIn = false
+let char;
+let value = null
+
+console.table([name1, age, isLogginIn,value])
+console.log(typeof undefined); // output is undefined
+console.log(typeof null); // output is object
+
