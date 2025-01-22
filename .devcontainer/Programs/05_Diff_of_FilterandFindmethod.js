2ce877c5a3dff90900198ace203d189477e07a93
PankajTurkar <43439911+PankajTurkar@users.noreply.github.com>
2025-01-22 18:10:22 +0000

    changes

--- .devcontainer/Programs/05_Diff_of_FilterandFindmethod.js
+++ .devcontainer/Programs/05_Diff_of_FilterandFindmethod.js
@@ -1,6 +1,7 @@
 /*what is difference between Filter and Find method?
-Ans -> filter() retunr the matched values in the array from the collection
-find() -> find() is retuen the first values that are matches from the array and then it will not check for remening values
+Ans -> filter() return the matched values in the array from the collection
+find() -> find() is return the first values that are matches from the array 
+and then it will not check for remening values
 
 https://www.youtube.com/watch?v=NRb1ymqg5dQ&list=PLAx7-E_inM6EkgZkrujZvewiM_QZRU4A2&index=5
 
