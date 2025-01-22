44af0a48d29702b0877edc1b8ebce86e71de4969
PankajTurkar <43439911+PankajTurkar@users.noreply.github.com>
2025-01-22 16:24:36 +0000

    js Programs

---
+++ .devcontainer/02_basics/03_objects.js
@@ -0,0 +1,4 @@
+//Objects in depth in javascript in hindi
+// object can be define in two ways Literal and constructor
+// //Singletone :- When we create Constructor then singletone (single) object is created.
+//
