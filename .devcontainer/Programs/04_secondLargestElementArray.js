f4dc2f468a0eb37e8f763a417619259f02b75d35
PankajTurkar <43439911+PankajTurkar@users.noreply.github.com>
2025-01-22 17:17:47 +0000

    second largest

--- .devcontainer/Programs/04_secondLargestElementArray.js
+++ .devcontainer/Programs/04_secondLargestElementArray.js
@@ -1,24 +1,7 @@
-// const arr = [1, 2, 3, 4, 55];
-// let max = arr[0]
-// let min = arr[0]
-
-// for (let i=0; i<=arr.length;i++){
-
-//     if(arr[i]>max){
-//         max = arr[i]
-//     }
-//     if(arr[i]<min){
-//         min = arr[i]
-//     }
-// }console.log("max value is ", max);
-// console.log("min value is ", min);
-
-
-
-
 const arr = [1, 2, 33, 4, 5];
-let largest = 0;
-let secondLargest = 0;
+
+let largest = -Infinity;
+let secondLargest = -Infinity;
 
 for (const num of arr) {
   if (num > largest) {
